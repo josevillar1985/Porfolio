52ed40ea0edf74b1d112908764f942d82ff6913f
Jose Villar <josevillar1985@gmail.com>
2026-01-07 01:04:18 +0100

    Inicio del portfolio

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import '@/assets/style.css'
 
-Vue.config.productionTip = false
+Vue.use(Vuetify)
 
 new Vue({
-  router,
-  render: h => h(App)
+  vuetify: new Vuetify(),
+  render: h => h(App),
 }).$mount('#app')
